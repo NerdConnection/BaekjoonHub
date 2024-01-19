5b8f0abfbc54f1b866e34abcc989bd9dd5384faa
Heo NamSu <100738404+NerdConnection@users.noreply.github.com>
2024-01-20 00:51:41 +0900

    Update 괄호.py

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -2,25 +2,19 @@ import sys
 n = int(input())
 for _ in range(n):
     li = list(sys.stdin.readline().strip())
-    check = 0
-    flag = 0
+    stack = []
     for word in li:
         if word == '(':
-            check += 1
+            stack.append('(')
         else :
-            check -= 1
-            if check < 0:
+            if not stack:
                 print("NO")
-                flag = 1
                 break
-    
-    if flag == 1:
-        pass
-    
-    else:                        
-        if check == 0:
+            stack.pop()
+    else:
+        if len(stack) == 0:
             print("YES")
-        else:
+        else :
             print("NO")
 
 
