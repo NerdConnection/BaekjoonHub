8caaf77ea187e47afa3d9179395816d7c9e241ee
Heo NamSu <100738404+NerdConnection@users.noreply.github.com>
2024-02-17 00:10:45 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,28 +1,29 @@
 import sys
 
+# 2 top-down
+import sys
+dp = [[[0]*21 for _ in range(21)] for _ in range(21)]
 
-def w(a,b,c):
+def w(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         return 1
-
-    elif a > 20 or b > 20 or c > 20:
+    if a > 20 or b > 20 or c > 20:
         return w(20, 20, 20)
 
-    else:
-        for x in range(1,a+1):
-            for y in range(1,b+1):
-                for z in range(1,c+1):  
-                    if x< y and y < z:
-                        dp[z][x][y] = dp[z-1][x][y] + dp[z-1][x][y-1] - dp[z][x][y-1]
-                    else:
-                        dp[z][x][y] = dp[z][x-1][y] + dp[z][x-1][y-1] + dp[z-1][x-1][y] - dp[z-1][x-1][y-1]
+    if dp[a][b][c]:
+        return dp[a][b][c]
 
-        return dp[c][a][b]
+    else:
+        if a < b < c:
+            dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+            return dp[a][b][c]
+        else:
+            dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+            return dp[a][b][c]
 
 while True:
-    a,b,c = map(int,(sys.stdin.readline().split()))
-    dp = [[[1 for _ in range(21)] for _ in range(21)] for _ in range(21)]
-    
+    a, b, c = map(int, sys.stdin.readline().rstrip().split())
     if a == -1 and b == -1 and c == -1:
-        break 
-    print("w({}, {}, {}) = {}".format(a,b,c,w(a,b,c)))
+        break
+    ans = w(a, b, c)
+    print("w(%d, %d, %d) = %d" %(a, b, c, ans))
