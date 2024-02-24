a5c64ac6dcd9ce0553f84521f18091069d2e95cf
Heo NamSu <100738404+NerdConnection@users.noreply.github.com>
2024-02-24 17:24:38 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 444 ms, Memory: 178752 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,24 +1,20 @@
 import sys
-
 s = list(sys.stdin.readline().rstrip())
 q = int(sys.stdin.readline())
-dic = {}
-for i,char in enumerate(s):
-    if char in dic.keys():
-        dic[char].append(i)
-    else:
-        dic[char] = [i]
+arr = [[0]*26 for _ in range(len(s))]
+
+arr[0][ord(s[0])-97] =1
+for i in range(1,len(s)):
+    arr[i][ord(s[i])-97] = 1
+    for j in range(26):
+        arr[i][j] += arr[i-1][j]
 
 for _ in range(q):
     a,l,r = sys.stdin.readline().split()
     l = int(l)
     r = int(r)
-    res = 0
-    if a in dic.keys():
-        li = dic[a]
-        for idx in li:
-            if l<= idx and idx<=r:
-                res += 1
-        print(res) 
+    if l > 0:
+        res = arr[r][ord(a)-97] - arr[l-1][ord(a)-97] 
     else:
-        print(res)
+        res = arr[r][ord(a)-97]
+    print(res)
