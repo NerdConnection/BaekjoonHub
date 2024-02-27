975096524f9e46cb161cb4ed89f7d06e09004e52
Heo NamSu <100738404+NerdConnection@users.noreply.github.com>
2024-02-27 14:35:44 +0900

    [Gold V] Title: 체스판 다시 칠하기 2, Time: 828 ms, Memory: 332272 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
@@ -5,42 +5,25 @@ arr = []
 for _ in range(n):
     arr.append(list(sys.stdin.readline().rstrip()))
 
-start_b = [[0]*m for _ in range(n)]
-start_w = [[0]*m for _ in range(n)]
-
-for i in range(n):
-    for j in range(m):
-        if i%2 == 0:
-            if j%2 == 0:
-                if arr[i][j] == 'B':
-                    start_w[i][j] = 1
-                else:
-                    start_b[i][j] = 1
-                    
-            else:
-                if arr[i][j] == 'B':
-                    start_b[i][j] = 1
-                else:
-                    start_w[i][j] = 1
-        else:
-            if j%2 == 0:
-                if arr[i][j] == 'B':
-                    start_b[i][j] = 1
-                else:
-                    start_w[i][j] = 1
-            else:
-                if arr[i][j] == 'B':
-                    start_w[i][j] = 1
-                else:
-                    start_b[i][j] = 1
-
 sum_b = [[0]*(m+1) for _ in range(n+1)]
 sum_w = [[0]*(m+1) for _ in range(n+1)]
 
-for i in range(1,n+1):
-    for j in range(1,m+1):
-        sum_b[i][j] = sum_b[i-1][j] + sum_b[i][j-1] - sum_b[i-1][j-1] + start_b[i-1][j-1]
-        sum_w[i][j] = sum_w[i-1][j] + sum_w[i][j-1] - sum_w[i-1][j-1] + start_w[i-1][j-1]
+for i in range(n):
+    for j in range(m):
+        if (i+j)%2 == 0:
+            if arr[i][j] == 'B':
+                sum_b[i+1][j+1] = 1 + sum_b[i][j+1] + sum_b[i+1][j] - sum_b[i][j]
+                sum_w[i+1][j+1] = sum_w[i][j+1] + sum_w[i+1][j] - sum_w[i][j]
+            else:
+                sum_b[i+1][j+1] = sum_b[i][j+1] + sum_b[i+1][j] - sum_b[i][j]
+                sum_w[i+1][j+1] = 1 + sum_w[i][j+1] + sum_w[i+1][j] - sum_w[i][j]
+        else:
+            if arr[i][j] == 'B':
+                sum_b[i+1][j+1] = sum_b[i][j+1] + sum_b[i+1][j] - sum_b[i][j]
+                sum_w[i+1][j+1] = 1 + sum_w[i][j+1] + sum_w[i+1][j] - sum_w[i][j]
+            else:
+                sum_b[i+1][j+1] = 1 + sum_b[i][j+1] + sum_b[i+1][j] - sum_b[i][j]
+                sum_w[i+1][j+1] = sum_w[i][j+1] + sum_w[i+1][j] - sum_w[i][j]
 
 res = float('inf')
 for i in range(k,n+1):
