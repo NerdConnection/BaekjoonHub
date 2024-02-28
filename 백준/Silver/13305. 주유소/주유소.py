ebce2f36358cd71954e65ee7dffd102d8a7ab7ef
Heo NamSu <100738404+NerdConnection@users.noreply.github.com>
2024-02-28 18:00:18 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 46236 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,15 +3,11 @@ input = sys.stdin.readline
 n = int(input())
 distance = list(map(int, input().split()))
 gas_value = list(map(int, input().split()))
-result = 0
-gas_value.pop() # 마지막 도시 gas value 삭제
+min_gas_value = gas_value[0]
+res = 0
 
-while distance:
-    x = gas_value.index(min(gas_value))
-    dis = 0
-    for i in range(x,len(distance)):
-        dis += distance[i]
-    result += min(gas_value) * dis
-    distance = distance[:x]
-    gas_value = gas_value[:x]
-print(result)
+for i in range(n-1):
+    if gas_value[i] < min_gas_value:
+        min_gas_value = gas_value[i]
+    res += min_gas_value * distance[i]
+print(res)
