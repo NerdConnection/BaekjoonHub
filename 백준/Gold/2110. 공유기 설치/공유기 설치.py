0e80dc35d3e809f70b166f6557f2cd35726bc74a
Heo NamSu <100738404+NerdConnection@users.noreply.github.com>
2024-03-11 16:25:00 +0900

    [Gold IV] Title: 공유기 설치, Time: 556 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,31 +1,30 @@
-N, C = map(int, input().split())
+import sys
+# 정답 풀이
+input = sys.stdin.readline
+n,c = map(int,(input().split()))
+house_loc =[]
+for _ in range(n):
+    house_loc.append(int(input()))
+house_loc.sort()
 
-arr = []
-for _ in range(N):
-    arr.append(int(input()))
-arr.sort()
-
-start = 1 # 공유기 거리 최소
-end = arr[-1] - arr[0] # 공유기 거리 최대
-result = 0
-
-# 재귀로 적절한 두 공유기 사이의 거리를 찾는다
-while (start <= end):
-    mid = (start + end) // 2 # 현재 공유기 거리
-    current = arr[0]
-    count = 1
-
-    # 공유기 설치 몇 대 할 수 있는지 체크
-    for i in range(1, len(arr)):
-        if arr[i] >= current + mid:
-            count += 1
-            current = arr[i]
-    # 공유기 설치 수가 목표 보다 크면 공유기 사이 거리 늘림
-    if count >= C:
-        start = mid + 1
-        result = mid
-    # 공유기 설치 수가 목표 보다 작으면 공유기 사이 거리 줄임
-    else:
-        end = mid - 1
-
-print(result)
+start = 1
+end = house_loc[-1] - house_loc[0]
+res = 0
+if c == 2:
+    print(end)
+else:
+    while start <= end:
+        mid = (start + end) // 2
+        current = house_loc[0]
+        temp = 1
+        for i in range(1,len(house_loc)):
+            if house_loc[i] >= mid + current:
+                temp += 1
+                current = house_loc[i]
+        
+        if temp < c:
+            end = mid - 1 
+        else:
+            start = mid + 1
+            res = mid
+    print(res)
