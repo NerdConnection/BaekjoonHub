c9df6d1e58034b8534f9c7e3c96040112ab3353b
Heo NamSu <100738404+NerdConnection@users.noreply.github.com>
2025-02-16 18:11:47 +0900

    [Gold IV] Title: 최단경로, Time: 284 ms, Memory: 9068 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -2,37 +2,36 @@
 #include <vector>
 #include <climits>
 #include <queue>
-using namespace std;
 
-vector<pair<int, int>>graph[20001];
-vector<int> d(20001, INT_MAX);
+using namespace std;
 int V, E, k;
+vector<pair<int, int>>graph[20001];
+vector<int>d(20001, INT_MAX);
 
 void dijkstra() {
-	priority_queue <pair<int, int>> pq;
+	priority_queue<pair<int, int>>pq;
 	d[k] = 0;
 	pq.push({ 0,k });
 
 	while (!pq.empty()) {
-		int current_distance = - pq.top().first;
-		int current_node = pq.top().second;
+		int current_node =  pq.top().second;
+		int current_node_distance = - pq.top().first;
 		pq.pop();
 
-		if (d[current_node] < current_distance) continue;
-		
+		if (d[current_node] < current_node_distance) continue;
+
 		for (int i = 0; i < graph[current_node].size(); i++) {
 			int next_node = graph[current_node][i].first;
-			int next_node_distance = graph[current_node][i].second + current_distance;
-			
+			int next_node_distance = graph[current_node][i].second + current_node_distance;
+
 			if (next_node_distance < d[next_node]) {
 				d[next_node] = next_node_distance;
 				pq.push({ -next_node_distance, next_node });
 			}
-
 		}
 	}
 
-	for (int i = 1; i < V+1; i++) {
+	for (int i = 1; i < V + 1; i++) {
 		if (d[i] == INT_MAX) cout << "INF\n";
 		else cout << d[i] << "\n";
 	}
@@ -44,7 +43,7 @@ int main() {
 	for (int i = 0; i < E; i++) {
 		int u, v, w;
 		cin >> u >> v >> w;
-		graph[u].push_back({ v, w });
+		graph[u].push_back({ v,w });
 	}
 	dijkstra();
 	return 0;
